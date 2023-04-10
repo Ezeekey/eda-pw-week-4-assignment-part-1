1e297f6ca617cdea91fd5de7f5ffa0a0f12ad67b
Zeke Taves <ezekielprof@gmail.com>
2023-04-10 14:00:49 -0500

    Successfully greeted, and added math functions.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,21 +15,24 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return "Hello " + name + "!";
 }
 // Remember to call the function to test
+console.log(helloName("Bob"));
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
-
+console.log("addNumbers = " + addNumbers(2, 5));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( x, y, z ){
+  return x * y * z;
 }
+console.log("multiplyThree = " + multiplyThree(2, 3, 4));
 
 
 // 5. Function that will return true if a number is positive, 
